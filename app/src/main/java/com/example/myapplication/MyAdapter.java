515d93f531376748fd6529fa9a61b6ee2144b309
Carole <carole.pichery@gmail.com>
2019-05-10 23:39:41 +0200

    changement de la couleur du background

--- app/src/main/java/com/example/myapplication/MyAdapter.java
+++ app/src/main/java/com/example/myapplication/MyAdapter.java
@@ -73,7 +73,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
             }
         });
 
-        holder.txtFooter.setText("Country of origin : " + selectedBeer.getCountry());
+        holder.txtFooter.setText("Type of beer : " + selectedBeer.getBrewery_type());
     }
 
     // Return the size of your dataset (invoked by the layout manager)
